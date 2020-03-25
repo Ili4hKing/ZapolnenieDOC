d29865e6d8248f13656f6d43f27751063f407e32
Илья Лугановский <ilya.luganovskiy@mail.ru>
2020-03-26 00:21:30 +0300

    Добавьте файлы проекта.

---
+++ ZapolnenieDOC/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ZapolnenieDOC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (IDbConnection connection = new SqlConnection())
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Word.Application wordApp = new Word.Application();// Создаём объект приложения
+
+            Word.Document Word = new Word.Document();
+
+
+            using (TexnikymBDEntities db = new TexnikymBDEntities())
+            {
+                var Bd_911 = db.Бд_911;
+                var I_913 = db.И_913;
+                var Ip_93 = db.Ип_93;
+                var M_92 = db.М_92;
+                var Mc_91 = db.Мц_91;
+                var Me_912 = db.Мэ_912;
+                var Ol_94 = db.Ол_94;
+                var Tv_914 = db.Тв_914;
+                var Students = db.Студенты2;
+                //var tex = db;
+                foreach (Студенты2 tl in Students)
+                {
+                    
+                    var name = db.Бд_911.Where(c =>c.ДатаРождения == tl.ДатаРождения).FirstOrDefault();
+                    if (name != null)
+                    {
+                        string sPersons = name.ФИО;
+                        string[] a = sPersons.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        string p = a[0] + " " + a[1] + " " + a[2];
+                    
+                    var customer = db.Бд_911.Where(c => tl.ФИО == p && c.ДатаРождения == tl.ДатаРождения).FirstOrDefault();
+                    if (customer != null)
+                    {
+                        if (customer.Паспорт == "")
+                        {
+                            customer.Паспорт = tl.ПаспортныеДанные;
+                            
+                        }
+                    }
+                    }
+                    //db.SaveChanges();
+
+                    //foreach (Бд_911 pl in Bd_911)
+                    //{
+                    //    if (tl.ФИО == pl.ФИО && tl.ДатаРождения == pl.ДатаРождения )
+                    //    {
+                    //        if(pl.Паспорт == "" )
+                    //        {
+                    //            pl.Паспорт = tl.ПаспортныеДанные;
+                    //            db.Бд_911.
+
+                    //        }
+
+                   
+
+
+                    //    }
+
+
+
+
+
+                    //}
+                    //foreach (И_913 rl in I_913)
+                    //{
+                    //    if (tl.ФИО == rl.ФИО && tl.ДатаРождения == rl.ДатаРождения)
+                    //    {
+                    //        if (rl.Паспорт == null)
+                    //        {
+                    //            rl.Паспорт = tl.ПаспортныеДанные;
+
+                    //        }
+
+
+
+
+                    //    }
+
+
+
+
+
+                    //}
+                    //foreach (Ип_93 ql in Ip_93)
+                    //{
+                    //    if (tl.ФИО == ql.ФИО && tl.ДатаРождения == ql.ДатаРождения)
+                    //    {
+                    //        if (ql.Паспорт == null)
+                    //        {
+                    //            ql.Паспорт = tl.ПаспортныеДанные;
+
+                    //        }
+
+
+
+
+                    //    }
+
+
+
+
+
+                    //}
+                    //foreach (М_92 wl in M_92)
+                    //{
+                    //    if (tl.ФИО == wl.ФИО && tl.ДатаРождения == wl.ДатаРождения)
+                    //    {
+                    //        if (wl.Паспорт == null)
+                    //        {
+                    //            wl.Паспорт = tl.ПаспортныеДанные;
+
+                    //        }
+
+
+
+
+                    //    }
+
+
+
+
+
+                    //}
+
+
+
+
+
+
+
+
+
+
+                }
+
+                db.SaveChanges();
+
+            }
+        }
+        //public struct Person
+        //{
+        //    public string Famaly
+        //    { get; set; }
+        //    public string Name
+        //    { get; set; }
+        //    public string Sername
+        //    { get; set; }
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string а  = "Гумбатова  Светлана Габиловна Перевод в гр Ол-94 с 26.09.19 пр. №58-К/д от 25.09.19";
+
+
+            //List<Person> persons = new List<Person>();
+            string sPersons = "Гумбатова-Тестова  Светлана Габиловна Перевод в гр Ол-94 с 26.09.19 пр. №58-К/д от 25.09.19";
+            string[] a = sPersons.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //for (int i = 0; i < a.Length;)
+            //{
+            //    Person person = new Person
+            //    {
+            //        Famaly = a[i++],
+            //        Name = a[i++],
+            //        Sername = a[i++]
+            //    };
+            //    persons.Add(person);
+            //}
+            string p = a[0] + " " + a[1] + " " + a[2];
+            MessageBox.Show(p);
+            //foreach (Person p in persons)
+            //    MessageBox.Show(string.Format("Фамилия: {0}\nИмя: {1}\nОтчество: {2}\n________________",
+            //        p.Famaly, p.Name, p.Sername));
+
+        }
+    }
+}
