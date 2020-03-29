6b44c786104c33a5978acaf7a56401c690891a00
Илья Лугановский <ilya.luganovskiy@mail.ru>
2020-03-29 21:08:46 +0300

    Фулл без лишних таблиц

--- ZapolnenieDOC/TexnikymDB.Context.cs
+++ ZapolnenieDOC/TexnikymDB.Context.cs
@@ -25,14 +25,6 @@ namespace ZapolnenieDOC
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<Бд_911> Бд_911 { get; set; }
-        public virtual DbSet<И_913> И_913 { get; set; }
-        public virtual DbSet<Ип_93> Ип_93 { get; set; }
-        public virtual DbSet<М_92> М_92 { get; set; }
-        public virtual DbSet<Мц_91> Мц_91 { get; set; }
-        public virtual DbSet<Мэ_912> Мэ_912 { get; set; }
-        public virtual DbSet<Ол_94> Ол_94 { get; set; }
-        public virtual DbSet<Тв_914> Тв_914 { get; set; }
         public virtual DbSet<ШаблонГруппы> ШаблонГруппы { get; set; }
         public virtual DbSet<Студенты2> Студенты2 { get; set; }
         public virtual DbSet<ШаблонСтудентов> ШаблонСтудентов { get; set; }
